442e81d5d05fa9ecae6a8ced0a572332ae9a8fb0
GGGodBG <nguuenthuonghaita081@gmail.com>
2025-04-09 18:33:59 +0700

    Update and rename SeaEventModule.luau to SeaEventModule.lua

---
+++ SeaEventModule.lua
@@ -0,0 +1,27 @@
+local getgenv = getgenv
+local success, getgenv = pcall(function()
+  return getgenv()
+end)
+local function CheckSeaBeast()
+    for i,v in pairs(game:GetService("WorkSpace").SeaBeasts:GetChildren()) do
+      if not workspace.SeaBeast then
+         return 
+      end
+       if v.Name == "Sea Beast" and v:FindFirstChild("HumanoidRootPart") then
+          CheckSeaBeast();
+          getgenv().CheckSea = true;
+      end
+   end
+end
+local function CheckLeviathan()
+ for i,v in pairs(game:GetService("WorkSpace").SeaBeasts:GetChildren()) do
+    if not workspace.SeaBeast then return end
+      if v.Name == "Leviathan" and v.Name == "Leviathan Segment1" and v:FindFirstChild("HumannoiRootPart") then
+          if v.Name ~= "Leviathan Segment" then 
+            return true
+         end
+          return false
+        end
+      end
+   end
+end
